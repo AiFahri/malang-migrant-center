75eace6ba521d805e1578cd1b4af21605379b468
AiFahri <fahrinurilll@gmail.com>
2026-09-24 00:10:56 +0700

    feat: update Welcome component to include additional public sections

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,4 +1,12 @@
+import AboutSection from '@/Components/Public/Home/AboutSection';
+import ContactSection from '@/Components/Public/Home/ContactSection';
+import CountrySection from '@/Components/Public/Home/CountrySection';
+import FaqSection from '@/Components/Public/Home/FaqSection';
+import InformationCenterSection from '@/Components/Public/Home/InformationCenterSection';
+import LatestInformationSection from '@/Components/Public/Home/LatestInformationSection';
+import MigrationStepsSection from '@/Components/Public/Home/MigrationStepsSection';
 import PublicHero from '@/Components/Public/PublicHero';
+import PublicFooter from '@/Components/Public/PublicFooter';
 import PublicHeader from '@/Components/Public/PublicHeader';
 import { Head } from '@inertiajs/react';
 
@@ -15,7 +23,15 @@ export default function Welcome() {
                 <PublicHeader />
                 <main id="main-content">
                     <PublicHero />
+                    <CountrySection />
+                    <MigrationStepsSection />
+                    <AboutSection />
+                    <LatestInformationSection />
+                    <InformationCenterSection />
+                    <FaqSection />
+                    <ContactSection />
                 </main>
+                <PublicFooter />
             </div>
         </>
     );
